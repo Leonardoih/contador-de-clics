38acae8604b0e0cb6e3e86530537b66d4f506554
leonardoih <leonardoih090217@gamil.com>
2022-03-22 23:16:59 -0500

    Update Jul 21/2016

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import './App.css';
+import freeCodeCampLogo from '../src/imagenes/freecodecamp-logo.png';
+import Boton from './componentes/Boton';
+
+function App() {
+  const manejarClic = () => {
+    console.log('se dio Clic');
+  };
+  const reiniciarContador = () => {
+    console.log('se dio en Reiniciar');
+  };
+
+  return (
+    <div className='App'>
+      <div className='freecodecamp-logo-contenedor'>logos2
+      
+        <img
+          className='freecodecamp-logo'
+          src={freeCodeCampLogo}
+          alt='Logo de freeCodeCamp'
+        />
+      </div>
+      <div className='contenedor-principal'>nuevos cambios
+        <Boton texto='Clic' esBotonDeClic={true} manejarClic={manejarClic} />
+        <Boton
+          texto='Reiniciar'
+          esBotonDeClic={false}
+          manejarClic={reiniciarContador}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default App;
