d619e7c5d635901a6ee6e3d7d4196fd6d65f14b9
leonardoih <leonardoih090217@gamil.com>
2022-03-24 22:29:12 -0500

    nuevos cambios

--- src/App.jsx
+++ src/App.jsx
@@ -4,39 +4,38 @@ import Boton from './componentes/Boton';
 import Contador from './componentes/Contador';
 
 function App() {
-  const manejarClic = () => {
-    console.log('se dio Clic');
-  };
-  const reiniciarContador = () => {
-    console.log('se dio en Reiniciar');
-  };
+	const manejarClic = () => {
+		console.log('se dio Clic');
+	};
+	const reiniciarContador = () => {
+		console.log('se dio en Reiniciar');
+	};
 
-  return (
-    <div className='App'>
-      <div className='freecodecamp-logo-contenedor'>
-      
-      
-        <img
-          className='freecodecamp-logo'
-          src={freeCodeCampLogo}
-          alt='Logo de freeCodeCamp'
-        />
-      </div>
-      <div className='contenedor-principal'>
-        <Contador numClics="5"></Contador>
-        <Boton texto='Clic' esBotonDeClic={true} manejarClic={manejarClic} />
-        <Boton
-          texto='Reiniciar'
-          esBotonDeClic={false}
-          manejarClic={reiniciarContador}
+	return (
+		<div className='App'>
+			<div className='freecodecamp-logo-contenedor'>
+				<img
+					className='freecodecamp-logo'
+					src={freeCodeCampLogo}
+					alt='Logo de freeCodeCamp'
+				/>
+			</div>
+			<div className='contenedor-principal'>
+				<Contador numClics='5'> </Contador>
 
-          
-
-
-        />
-      </div>
-    </div>
-  );
+				<Boton
+					texto='Clic'
+					esBotonDeClic={true}
+					manejarClic={manejarClic}
+				/>
+				<Boton
+					texto='Reiniciar'
+					esBotonDeClic={false}
+					manejarClic={reiniciarContador}
+				/>
+			</div>
+		</div>
+	);
 }
 
 export default App;
